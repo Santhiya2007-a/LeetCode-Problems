89a63cdf225efb76d010d8236bfcc15ecfc53b1b
Santhiya Muniappan <sandymunijaya127@gmail.com>
2026-06-10 22:11:29 +0530

    Add files via upload

---
+++ 3sum_closest.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        nums.sort()
+        closest = float('inf')
+
+        for i in range(len(nums)):
+            l,r = i + 1,len(nums) - 1
+
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+
+                if abs(target - s) < abs(target - closest):
+                    closest = s
+
+                if s < target:
+                    l += 1
+                elif s > target:
+                    r -= 1
+                else:
+                    return s
+
+        return closest
+        
