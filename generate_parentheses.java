476563a74cd619f3cf1643ce4526e6b8e8cdb330
Santhiya Muniappan <sandymunijaya127@gmail.com>
2026-06-11 20:55:12 +0530

    Add files via upload

---
+++ generate_parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        solve(res,"",0,0,n);
+        return res;
+    }
+    void solve(List<String> res,String s,int open,int close,int n){
+        if(s.length() == 2 * n){
+            res.add(s);
+            return;
+        }
+
+        if (open < n) solve(res,s + "(", open + 1,close,n);
+        if (close < open) solve(res,s + ")",open,close + 1,n);
+    }
+}
