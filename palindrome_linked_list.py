be70a5d80542ea0773d3f594b3ccc3b8009df249
Santhiya Muniappan <sandymunijaya127@gmail.com>
2026-06-21 19:25:56 +0530

    Add files via upload

---
+++ palindrome_linked_list.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, head):
+        arr = []
+
+        while head:
+            arr.append(head.val)
+            head = head.next
+
+        return arr == arr[::-1]
